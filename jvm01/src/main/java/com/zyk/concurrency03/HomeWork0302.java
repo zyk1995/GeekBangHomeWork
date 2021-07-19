3724454f2cb2bada559d4a5d1e048d3c03e549b9
DESKTOP-75UBMJT\zyk <1251134350@qq.com>
2021-07-19 20:51:09 +0800

    作业week4

--- jvm01/src/main/java/com/zyk/concurrency03/HomeWork0302.java
+++ jvm01/src/main/java/com/zyk/concurrency03/HomeWork0302.java
@@ -1,24 +1,20 @@
 package com.zyk.concurrency03;
 
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.FutureTask;
 
-/**
- * 本周作业：（必做）思考有多少种方式，在main函数启动一个新线程或线程池，
- * 异步运行一个方法，拿到这个方法的返回值后，退出主线程？
- * 写出你的方法，越多越好，提交到github。
- *
- * 一个简单的代码参考：
- */
-public class Homework0301 {
-    
+public class HomeWork0302 {
+
     public static void main(String[] args) throws ExecutionException, InterruptedException {
-        
+
         long start=System.currentTimeMillis();
 
         // 在这里创建一个线程或线程池，
-        FutureTask<Integer> futureTask = new FutureTask<>(Homework0301::sum);
-        new Thread(futureTask).start();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        FutureTask<Integer> futureTask = new FutureTask<>(HomeWork0302::sum);
+        executor.submit(futureTask);
 
         // 异步执行 下面方法
 
@@ -26,19 +22,19 @@ public class Homework0301 {
 
         // 确保  拿到result 并输出
         System.out.println("异步计算结果为："+result);
-         
-        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
-        
-        // 然后退出main线程
 
+        System.out.println("使用时间："+ (System.currentTimeMillis()-start) + " ms");
+
+        // 然后退出main线程
+        executor.shutdown();
     }
-    
+
     private static int sum() {
         return fibo(36);
     }
-    
+
     private static int fibo(int a) {
-        if ( a < 2) 
+        if ( a < 2)
             return 1;
         return fibo(a-1) + fibo(a-2);
     }
